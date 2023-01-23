fed943c163f30528e40a5582bdc77cdbcdaded16
developmentHF <development.henry@gmail.com>
2023-01-23 13:34:38 +1100

    initial main setup

--- main.lua
+++ main.lua
@@ -3,3 +3,17 @@
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
+local composer = require("composer")
+local widget = require("widget")
+
+-- Hides the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- Seed the random number generation for potential RNG uses
+math.randomseed(os.time())
+
+-- Go to the menu screen
+composer.gotoScene("menu")
+
+-- Enable multitouch so two players can play at once
+system.activate("multitouch")
